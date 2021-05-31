03db4c50db716bd89891932fb50f8f3012d670f0
Delfi <delfimassa@gmail.com>
2021-05-31 19:17:27 -0300

    primer commit

---
+++ js/holamundo.js
@@ -0,0 +1,26 @@
+console.log("hola mundo");
+// alert("Hola mundo!");
+document.write("<h1>Hola mundo!</h1>");
+
+
+//declaracion de variables var let const
+// varible de texto  o string
+let nombre = "Delfina";
+// variable numerica
+let edad1 = 24;
+let edad2 = 13;
+let difEdad = edad1 - edad2 ;
+document.write(difEdad);
+document.write("Hola "+nombre);
+nombre = "Pablo";
+document.write("<br>Hola "+nombre);
+
+let nombreUsuario =prompt("Cual es tu nombre?");
+console.log(nombreUsuario);
+document.write("<br>Hola "+nombreUsuario);
+
+let numero1 = prompt("Ingrese un numero");
+let numero2 = prompt("Ingrese otro numero");
+let suma = parseInt(numero1)+parseInt(numero2)
+document.write("El resultado es: "+suma);
+
---
+++ js/holamundo2.js
@@ -0,0 +1,3 @@
+console.log("hola mundo");
+// alert("Hola mundo!");
+document.write("<h1>Hola mundo!</h1>");
